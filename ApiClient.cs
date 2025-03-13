86261b570cddb94377009de73aafb4804eed9371
Far-Kamil <kamil200523@gmail.com>
2025-03-13 17:57:19 +0300

    Add files via upload

---
+++ ApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_task
+{
+    class ApiClient: IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        public ApiClient(string baseURL)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseURL)
+            };
+        }
+        public async Task<string> GetAsync(string endpoint, string Params = "")
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"{endpoint}/{Params}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        public void Dispose() 
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
---
+++ RandomService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _6_task
+{
+    class RandomService: IDisposable
+    {
+        private const string baseURL= "http://www.randomnumberapi.com/";
+        private const string pattern = @"\d+";
+        private ApiClient client;
+        private string endpoint = "api/v1.0/random";
+        public RandomService() 
+        {
+            client=new ApiClient(baseURL);
+        }
+        public async Task<int> Next(int maxvalue) 
+        {
+            return await Next(0, maxvalue);
+        }
+        public async Task<int> Next(int minvalue, int maxvalue)
+        {
+            var Params = $"?min={minvalue}&max={maxvalue}";
+            var response = await client.GetAsync(endpoint, Params);
+            var matches = Regex.Match(response, pattern);
+            return Int32.Parse(matches.Groups[0].Value);
+        }
+        public void Dispose() 
+        {
+            client.Dispose();
+        } 
+    }
+}
