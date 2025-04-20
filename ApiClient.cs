e02c8898644154492b63a837b8b580107fb1bff4
Far-Kamil <kamil200523@gmail.com>
2025-04-20 19:30:47 +0300

    Add files via upload

---
+++ ApiClient.cs
@@ -0,0 +1,24 @@
+﻿namespace _9_task
+{
+    public class ApiClient : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        public ApiClient(string baseUrl)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+        }
+        public async Task<string> GetAsync(string endpoint, string Params = "")
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"{endpoint}/{Params}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
